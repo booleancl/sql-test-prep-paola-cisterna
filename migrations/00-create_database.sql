7c56d67bb2b5c3cddafd9588078941e9e8d5fc01
aylinsanmartin <a.sanlazo@gmail.com>
2022-10-21 11:07:56 -0300

    ejercicio sql

--- migrations/00-create_database.sql
+++ migrations/00-create_database.sql
@@ -1,1 +1,3 @@
 -- Crear la base de datos ecommerce
+DROP DATABASE IF EXISTS ecommerce;
+CREATE DATABASE ecommerce;
--- migrations/02-create_products_table.sql
+++ migrations/02-create_products_table.sql
@@ -2,5 +2,12 @@
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products(
+    id INTEGER NOT NULL,
+    name VARCHAR(50),
+    sku VARCHAR(15),
+    price INT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+PRIMARY KEY(id)
   
 )
